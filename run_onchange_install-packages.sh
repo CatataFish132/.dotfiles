83d063c0ceb7dab78992b46c7a6b69035c3eca40
Thimo Veldema <thimoveldema@gmail.com>
2025-01-27 16:14:54 +0100

    init commit

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git submodule update --init --recursive
+sudo pacman -Syu pyenv fish pass picom blueman network-manager-applet pulseaudio pulseaudio-bluetooth pavucontrol kitty firefox rofi rofi-pass awesome vim ttf-firacode-nerd
