bae71fdc38fa287315fe547447dfaa98e5e20278
Thimo Veldema <thimoveldema@gmail.com>
2025-01-29 15:04:16 +0100

    added pyenv

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -Syu pyenv fish pass picom blueman network-manager-applet pulseaudio pulseaudio-bluetooth pavucontrol kitty firefox rofi rofi-pass awesome vim ttf-firacode-nerd dex
+sudo pacman -Syu pyenv fish pass picom blueman network-manager-applet pulseaudio pulseaudio-bluetooth pavucontrol kitty firefox rofi rofi-pass awesome vim ttf-firacode-nerd dex pyenv
