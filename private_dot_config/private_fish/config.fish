33b5327720335a92cd87812bb0d20566d2544c20
Thimo Veldema <thimoveldema@gmail.com>
2025-04-28 20:25:24 +0200

    fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,3 +3,10 @@ if status is-interactive
     pyenv init - | source
 end
 
+# Get unread count
+set unread_count (newsboat -x print-unread | awk '{print $1}')
+
+# If unread count is greater than 0, print it
+if test $unread_count -gt 0
+    echo $unread_count 'Unread articles in newsboat' 
+end
