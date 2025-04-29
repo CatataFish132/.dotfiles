a8823aefdeeaacab56c925fa91b7e10c8af0bc6a
Thimo Veldema <thimoveldema@gmail.com>
2025-04-29 16:48:25 +0200

    sway

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git submodule update --init --recursive
-sudo pacman -Syu pyenv fish pass picom blueman network-manager-applet pulseaudio pulseaudio-bluetooth pavucontrol kitty firefox rofi rofi-pass awesome vim ttf-firacode-nerd xorg xorg-init luarocks dex pyenv
+sudo pacman -Syu pyenv fish pass picom blueman network-manager-applet pulseaudio pulseaudio-bluetooth pavucontrol kitty firefox rofi rofi-pass awesome vim ttf-firacode-nerd xorg luarocks dex pyenv otf-font-awesome sway waybar
